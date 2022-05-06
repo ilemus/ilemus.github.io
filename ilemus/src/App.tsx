7a22f0c3f35142c3b4d7103aab972b33abde33f2
Isaac Lemus <ilemus@umich.edu>
2022-05-06 10:41:58 -0500

    switch to typescript

---
+++ ilemus/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Application from './components/Application';
+
+function App() {
+  return (
+    <React.Fragment>
+        <CssBaseline />
+        <Application />
+    </React.Fragment>
+  );
+}
+
+export default App;
