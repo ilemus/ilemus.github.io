f5dad77266b8e88e7e494c0c284340e780520c7d
ilemus <ilemus@umich.edu>
2022-05-14 14:45:07 -0500

    working on drawer

--- ilemus/src/components/Application.tsx
+++ ilemus/src/components/Application.tsx
@@ -1,9 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -44,10 +50,47 @@ function LoginOrSignUp(props: {sx?: object}) {
 }
 
 export default function Application(props: { userLoginId?: number}) {
-    const [loggedId, setLoggedIn] = useState(false);
-    useEffect(() => {
+    const [loggedId, setLoggedIn] = React.useState(false);
+    const [state, setState] = React.useState(false);
+    React.useEffect(() => {
         setLoggedIn(props.userLoginId !== undefined && props.userLoginId !== null);
     }, [setLoggedIn, props.userLoginId]);
+
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event &&
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+          return;
+        }
+    
+        setState(open);
+      };
+
+    // Could be a dynamically constructed list of elements which are defined as an array elsewhere.
+    const list = () => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+            >
+            <List>
+            <ListItem key='image-upload' disablePadding>
+            <ListItemButton onClick={toggleDrawer(false)}
+                            onKeyDown={toggleDrawer(false)}>
+                {/* 
+                <ListItemIcon>
+                {}
+                </ListItemIcon>
+                */}
+                <ListItemText primary='Image Upload' />
+            </ListItemButton>
+            </ListItem>
+            </List>
+        </Box>
+    )
 
 
     return (
@@ -58,8 +101,18 @@ export default function Application(props: { userLoginId?: number}) {
                                 edge="start"
                                 color="inherit"
                                 aria-label="open drawer"
+                                onClick={toggleDrawer(true)}
+                                onKeyDown={toggleDrawer(true)}
                                 sx={{ mr: 2 }} >
                         <MenuIcon />
+                        <SwipeableDrawer
+                            anchor='left'
+                            open={state}
+                            onClose={toggleDrawer(false)}
+                            onOpen={toggleDrawer(true)}
+                        >
+                            {list()}
+                        </SwipeableDrawer>
                     </IconButton>
                     <Typography variant="h6"
                                 noWrap
