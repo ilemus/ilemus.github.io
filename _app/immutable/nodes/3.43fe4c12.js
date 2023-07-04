0df4cb25df34746ed006435a3259daff17ee2dc2
lemusi <lemusi@proton.me>
2023-07-04 18:08:39 -0500

    adding property managers

---
+++ _app/immutable/nodes/3.43fe4c12.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.d3154425.js";import{S as t,i as e}from"../chunks/index.a7ecb635.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
