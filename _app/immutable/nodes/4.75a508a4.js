e980ba27570451405b0e17fa54430c7e4c5a71d8
lemusi <lemusi@proton.me>
2023-07-04 23:42:15 -0500

    adding more interactions

---
+++ _app/immutable/nodes/4.75a508a4.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.2df72c25.js";import{S as t,i as e}from"../chunks/index.f9d86744.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
