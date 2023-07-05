586604fb7d62cd6c0f356a5d07ad27c1e07db679
lemusi <lemusi@proton.me>
2023-07-04 21:38:55 -0500

    adding slider

---
+++ _app/immutable/nodes/3.06c70db2.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.2df72c25.js";import{S as t,i as e}from"../chunks/index.5f767e86.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
--- _app/immutable/nodes/7.e80ab931.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.2df72c25.js";import{S as t,i as e}from"../chunks/index.9f4209a8.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
