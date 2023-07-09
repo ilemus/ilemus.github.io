0eb93873f4e749ca88bf26c65476cbcbfcdb927f
lemusi <lemusi@proton.me>
2023-07-09 16:22:51 -0500

    update

--- README.md
+++
@@ -1,5 +0,0 @@
-# ilemus.github.io
-SvelteKit App
-
-### Version
-0.0.3
--- _app/immutable/nodes/4.75a508a4.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.2df72c25.js";import{S as t,i as e}from"../chunks/index.f9d86744.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
